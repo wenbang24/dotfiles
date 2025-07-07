2ef239535990c1340c459f36fb495f92bef3f7f8
wenbang24 <wenbangalt@gmail.com>
2025-07-07 21:25:24 +1000

    nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,12 +4,11 @@ require('flit').setup {
   keys = { f = 'f', F = 'F', t = 't', T = 'T' },
   -- A string like "nv", "nvo", "o", etc.
   labeled_modes = "v",
-  -- Repeat with the trigger key itself.
   clever_repeat = true,
   multiline = true,
-  -- Like `leap`s similar argument (call-specific overrides).
-  -- E.g.: opts = { equivalence_classes = {} }
   opts = {}
 }
 
 vim.opt.shiftwidth = 4
+vim.cmd.colorscheme "catppuccin"
+vim.cmd.set "relativenumber"
